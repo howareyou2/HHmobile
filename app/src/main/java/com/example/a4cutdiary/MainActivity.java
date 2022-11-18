cd81efc688c3dd26bb75a900053750008d9dbc9a
j2j2 <fineimagination@gmail.com>
2022-11-18 11:50:05 +0900

    first_Album

--- app/src/main/java/com/example/a4cutdiary/MainActivity.java
+++ app/src/main/java/com/example/a4cutdiary/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.a4cutdiary;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -14,6 +19,7 @@ import com.google.android.material.navigation.NavigationBarView;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     HomeFragment HFragment;
     MapFragment MFragment;
     AlbumFragment AFragment;
+    DiaryFragment DFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         HFragment = new HomeFragment();
         MFragment = new MapFragment();
         AFragment = new AlbumFragment();
+        DFragment = new DiaryFragment();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, HFragment).commit();
 
@@ -55,7 +63,47 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void onFragmentChanged(int index){
+        if(index==0){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,DFragment).commit();
+        }else if(index==1){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,AFragment).commit();
+        }
+    }
+
+    public void AddNewPhoto(){
+        openGallery();
+    }
+
+    public void openGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent, 101);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==101){
+            if(resultCode==RESULT_OK){
+                Uri fileUri = data.getData();
+
+                ContentResolver resolver = getContentResolver();
+
+                try{
+
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
 
 
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
